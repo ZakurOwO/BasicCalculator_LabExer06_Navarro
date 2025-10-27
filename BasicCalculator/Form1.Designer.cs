615d443c9b2a9d856c19387636f687999bba6a01
ZakurOwO <johndanielnavarro130@gmail.com>
2025-10-27 19:17:28 +0800

    Finished the entire backend.

--- BasicCalculator/Form1.Designer.cs
+++ BasicCalculator/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.TextBoxInput1 = new System.Windows.Forms.RichTextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ComboBoxData = new System.Windows.Forms.ComboBox();
             this.TextBoxInput2 = new System.Windows.Forms.RichTextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.ResultTotal = new System.Windows.Forms.Label();
+            this.ComputeBTN = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,6 +52,7 @@
             // 
             // TextBoxInput1
             // 
+            this.TextBoxInput1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TextBoxInput1.Location = new System.Drawing.Point(22, 59);
             this.TextBoxInput1.Name = "TextBoxInput1";
             this.TextBoxInput1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
@@ -58,17 +60,25 @@
             this.TextBoxInput1.Size = new System.Drawing.Size(349, 51);
             this.TextBoxInput1.TabIndex = 1;
             this.TextBoxInput1.Text = "";
+            this.TextBoxInput1.TextChanged += new System.EventHandler(this.TextBoxInput1_TextChanged);
             // 
-            // comboBox1
+            // ComboBoxData
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(132, 116);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 2;
+            this.ComboBoxData.FormattingEnabled = true;
+            this.ComboBoxData.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.ComboBoxData.Location = new System.Drawing.Point(132, 116);
+            this.ComboBoxData.Name = "ComboBoxData";
+            this.ComboBoxData.Size = new System.Drawing.Size(121, 21);
+            this.ComboBoxData.TabIndex = 2;
+            this.ComboBoxData.SelectedIndexChanged += new System.EventHandler(this.ComboBoxData_SelectedIndexChanged);
             // 
             // TextBoxInput2
             // 
+            this.TextBoxInput2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold);
             this.TextBoxInput2.Location = new System.Drawing.Point(22, 143);
             this.TextBoxInput2.Name = "TextBoxInput2";
             this.TextBoxInput2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
@@ -76,6 +86,7 @@
             this.TextBoxInput2.Size = new System.Drawing.Size(349, 51);
             this.TextBoxInput2.TabIndex = 3;
             this.TextBoxInput2.Text = "";
+            this.TextBoxInput2.TextChanged += new System.EventHandler(this.TextBoxInput2_TextChanged);
             // 
             // pictureBox1
             // 
@@ -109,18 +120,30 @@
             this.ResultTotal.Size = new System.Drawing.Size(98, 31);
             this.ResultTotal.TabIndex = 6;
             this.ResultTotal.Text = "000000";
+            this.ResultTotal.Click += new System.EventHandler(this.ResultTotal_Click);
+            // 
+            // ComputeBTN
+            // 
+            this.ComputeBTN.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComputeBTN.Location = new System.Drawing.Point(135, 314);
+            this.ComputeBTN.Name = "ComputeBTN";
+            this.ComputeBTN.Size = new System.Drawing.Size(110, 31);
+            this.ComputeBTN.TabIndex = 7;
+            this.ComputeBTN.Text = "Compute";
+            this.ComputeBTN.UseVisualStyleBackColor = true;
+            this.ComputeBTN.Click += new System.EventHandler(this.ComputeBTN_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::BasicCalculator.Properties.Resources.verdant_mix_paints_paper;
-            this.ClientSize = new System.Drawing.Size(381, 326);
+            this.ClientSize = new System.Drawing.Size(381, 357);
+            this.Controls.Add(this.ComputeBTN);
             this.Controls.Add(this.ResultTotal);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.TextBoxInput2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ComboBoxData);
             this.Controls.Add(this.TextBoxInput1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -135,11 +158,12 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.RichTextBox TextBoxInput1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox ComboBoxData;
         private System.Windows.Forms.RichTextBox TextBoxInput2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label ResultTotal;
+        private System.Windows.Forms.Button ComputeBTN;
     }
 }
 
--- BasicCalculator/Form1.cs
+++ BasicCalculator/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CalculatorPrivateAssembly2;
+
 
 namespace BasicCalculator
 {
@@ -15,6 +17,53 @@ namespace BasicCalculator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void TextBoxInput1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBoxData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxInput2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResultTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComputeBTN_Click(object sender, EventArgs e)
+        {
+            float num1 = float.Parse(TextBoxInput1.Text);
+            float num2 = float.Parse(TextBoxInput2.Text);
+            string operation = ComboBoxData.Text;
+            float result = 0;
+
+            if (operation == "+")
+            {
+                result = BasicComputation.Add(num1, num2);
+            }
+            else if (operation == "-")
+            {
+                result = BasicComputation.Subtract(num1, num2);
+            }
+            else if (operation == "*")
+            {
+                result = BasicComputation.Multiply(num1, num2);
+            }
+            else if (operation == "/")
+            {
+                result = BasicComputation.Divide(num1, num2);
+            }
+
+            ResultTotal.Text = result.ToString();
         }
     }
 }
